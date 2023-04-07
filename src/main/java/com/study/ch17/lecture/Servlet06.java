2befc64eae0a3a435b5a17fb5c830c99a8a3e403
dongukkoo <123609752+dongukkoo@users.noreply.github.com>
2023-04-07 16:43:45 +0900

    servlet

---
+++ src/main/java/com/study/ch17/lecture/Servlet06.java
@@ -0,0 +1,51 @@
+package com.study.ch17.lecture;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet06
+ */
+@WebServlet("/lec/sample06")
+public class Servlet06 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet06() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("res", "attribute!!!!!");
+		
+		String view = "/ch17/lecture/view04.jsp";
+		request.getRequestDispatcher(view).forward(request, response);
+		
+		
+//		// request attribute 추가 (모델에 데이터 담기)
+//		request.setAttribute("result", "일한 결과 데이터!");
+//		// view로 포워드
+//		String view = "/ch17/lecture/view03.jsp";
+//		request.getRequestDispatcher(view).forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
